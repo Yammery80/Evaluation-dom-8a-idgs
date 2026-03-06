b995427656e1f485e39efcc0ea45a11e90ab7bda
Yammery80 <yammery.28@gmail.com>
2026-03-06 11:12:06 -0600

    Agregar nuevas tareas

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// Declaración de utilidade s y referencias
+const $ = (sel, root = document) => root.querySelector(sel);
+const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
+
+// Referencias a elementos del DOM
+
+const form = $('#formTarea');
+const inputTitulo = $('#inputTitulo');
+const selectTag = $('#selectTag');
+
+const listaTareas = $('#listaTareas');
+
+//Agregar nuevas tareas
+
+const buildTarea = ({title, tag}) => {
+
+    const li = document.createElement('li');
+
+    li.className = 'card';
+
+    li.dataset.id = 't' + Date.now();
+    li.dataset.tag = tag;
+    li.dataset.fav = '0';
+
+    li.innerHTML = `
+        <div class="card__head">
+            <span class="badge">${tag}</span>
+
+            <div class="actions">
+                <button class="icon" data-action="fav">☆</button>
+                <button class="icon" data-action="done">✓</button>
+                <button class="icon danger" data-action="del">🗑</button>
+            </div>
+        </div>
+
+        <p class="card__title">${title}</p>
+    `;
+
+    return li;
+};
+
+
+
+//Agregar tarea funcion
+
+form.addEventListener('submit', (e)=>{
+
+    e.preventDefault();
+
+    const title = inputTitulo.value.trim();
+    const tag = selectTag.value;
+
+    if(!title) return;
+
+    const task = buildTarea({title, tag});
+
+    listaTareas.prepend(task);
+
+    inputTitulo.value = '';
+    inputTitulo.focus();
+
+    applyFilters();
+    updateStats();
+});
